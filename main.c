f2be18bd0f4bf88530a38db06e8eed09243f09c7
GradyRuefferOutlook <gradruef152@outlook.com>
2025-01-23 15:00:13 -0500

    Quick fix

--- main.c
+++ main.c
@@ -3,5 +3,5 @@
 int main()
 {
     int age = 2, b = 3, c = 5, d = 7;
-    printf("The sum of a, b, c, and d is: %d\n", age + b + c + d);
+    printf("The sum of age, b, c, and d is: %d\n", age + b + c + d);
 }
