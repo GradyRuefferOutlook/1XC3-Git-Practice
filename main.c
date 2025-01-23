837e8ceba04a04709334d2e63a298ba1b8b864de
GradyRuefferOutlook <gradruef152@outlook.com>
2025-01-23 15:16:51 -0500

    Testing for hidden files

--- main.c
+++ main.c
@@ -4,4 +4,5 @@ int main()
 {
     int age = 2, score = 3, c = 5, d = 7;
     printf("The sum of age, score, c, and d is: %d\n", age + score + c + d);
+    printf("I've hidden a file from github :)\n");
 }
