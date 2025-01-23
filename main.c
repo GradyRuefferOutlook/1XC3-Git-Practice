93e141dda5f467e513d90382d86a5a623dbf651b
GradyRuefferOutlook <gradruef152@outlook.com>
2025-01-23 15:02:03 -0500

    Changed b to score for all b

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-    int a = 2, b = 3, c = 5, d = 7;
-    printf("The sum of a, b, c, and d is: %d\n", a + b + c + d);
+    int a = 2, score = 3, c = 5, d = 7;
+    printf("The sum of a, score, c, and d is: %d\n", a + score + c + d);
 }
